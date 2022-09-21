936bae7ee6df0f9f8505e4eabbe39c77ab3ccbf9
Pankti <pankti77dave@gmail.com>
2022-09-21 13:07:34 +0530

    added slick carousel

---
+++ assets/js/general.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+    jQuery('.explore-section-inner').slick({
+        autoplay: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+    });
+});
