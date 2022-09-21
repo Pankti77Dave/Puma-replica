7d42200e5034dbe6ea49d55f529e762c6b62967d
Pankti <pankti77dave@gmail.com>
2022-09-21 19:01:34 +0530

    added footer and other sections

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,8 +1,18 @@
+function PaddingTopBanner(){
+    var headerHeight = jQuery('.header').innerHeight();
+    console.log(headerHeight);
+    jQuery('.banner-section').css("padding-top", headerHeight);
+}
 jQuery(document).ready(function(){
+    PaddingTopBanner();
+    //slick-slider
     jQuery('.explore-section-inner').slick({
         autoplay: true,
         slidesToShow: 4,
         slidesToScroll: 1,
         arrows: true,
     });
+});
+jQuery(window).resize(function(){
+    PaddingTopBanner();
 });
